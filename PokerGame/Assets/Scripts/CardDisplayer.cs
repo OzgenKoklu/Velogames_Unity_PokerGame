cee4432ec2dc7ba6b6550e593a3e72dc274d2a52
Ozgen Koklu <ozgen.koklu@gmail.com>
2024-03-26 14:08:40 +0300

    Test system added

--- PokerGame/Assets/Scripts/CardDisplayer.cs
+++ PokerGame/Assets/Scripts/CardDisplayer.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class CardDisplayer : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private CardSO _cardSO;
-    [SerializeField] private Texture2D _cardSheet;
+    // [SerializeField] private CardSO _cardSO;
+    [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private DeckSO _deckSO;
+
+    private void Awake()
+    {
+        int i = 0;
+        foreach (var cardSO in _deckSO.Cardlist)
+        {
+            var card = Instantiate(_cardPrefab);
+            card.GetComponent<SpriteRenderer>().sprite = cardSO.CardSprite;
+            card.transform.position = new Vector2(i, 0);
+            i++;
+        }
+    }
 }
