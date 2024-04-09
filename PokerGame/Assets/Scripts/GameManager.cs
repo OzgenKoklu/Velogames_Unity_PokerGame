7e305186035ea600d56518aed966c602fc2f2a8b
Ozgen Koklu <ozgen.koklu@gmail.com>
2024-04-09 12:36:15 +0300

    migrated Deck and card dealing logic from game manager to Poker deck manager

--- PokerGame/Assets/Scripts/GameManager.cs
+++ PokerGame/Assets/Scripts/GameManager.cs
@@ -34,14 +34,6 @@ public class GameManager : MonoBehaviour
     public List<PlayerManager> Players => _players;
     [SerializeField] private List<PlayerManager> _players;
 
-    [SerializeField] private CommunityCards _communityCards;
-    [SerializeField] private PokerPlayerHand _playerHand;
-    [SerializeField] private PokerPlayerHand _aiPlayerOneHand;
-    [SerializeField] private PokerPlayerHand _aiPlayerTwoHand;
-    [SerializeField] private PokerPlayerHand _aiPlayerThreeHand;
-    [SerializeField] private PokerPlayerHand _aiPlayerFourHand;
-    private List<PokerPlayerHand> _playerHands;
-
     private void Awake()
     {
         if (Instance == null)
@@ -56,19 +48,13 @@ public class GameManager : MonoBehaviour
     }
 
     private void Start()
-    {
-        InitializePlayers();
+    {    
         StartGame();
-        DrawInitialCommunityCards();
-        PokerHandEvaluator.WinningHandResults winningHandResult = PokerHandEvaluator.Instance.EvaluateAndFindWinner(GetAllPlayerHands());
-        Debug.Log("Winning hand type: " + winningHandResult.WinningHandType + "- Player Index(0,1,2,3,4), 0 is the player. : " + winningHandResult.WinningHandIndex + " - Winning Hand(5Cards) Ranks: " + winningHandResult.WinningCardCodes);
-
     }
 
     public void StartGame()
     {
         _isGameStarted = true;
-        DealCardsToPlayers();
         StartGameRound();
         OnGameStarted?.Invoke();
         Debug.Log("Ongamestarted");
@@ -93,85 +79,5 @@ public class GameManager : MonoBehaviour
     public GameState GetState()
     {
         return _currentGameState;
-    }
-
-    private void DrawInitialCommunityCards()
-    {
-        CardSO card1 = DrawCardFromDeck();
-        CardSO card2 = DrawCardFromDeck();
-        CardSO card3 = DrawCardFromDeck();
-
-        if (card1 != null)
-        {
-            _communityCards.AddCard(card1);
-            CardVisualsManager.Instance.SpawnCardObject(card1, card1.CardParent);
-
-        }
-        if (card2 != null)
-        {
-            _communityCards.AddCard(card2);
-            CardVisualsManager.Instance.SpawnCardObject(card2, card2.CardParent);
-        }
-        if (card3 != null)
-        {
-            _communityCards.AddCard(card3);
-            CardVisualsManager.Instance.SpawnCardObject(card3, card3.CardParent);
-        }
-    }
-
-    private void DealCardsToPlayers()
-    {
-        //Draws 2 cards for each player. 
-        foreach (PokerPlayerHand hand in _playerHands)
-        {
-            CardSO card1 = DrawCardFromDeck(); // Draw the first card
-            CardSO card2 = DrawCardFromDeck(); // Draw the second card
-
-            if (card1 != null)
-                hand.AddCard(card1); // Add the first card to the player's hand
-            CardVisualsManager.Instance.SpawnCardObject(card1, card1.CardParent);
-
-            if (card2 != null)
-                hand.AddCard(card2); // Add the second card to the player's hand
-            CardVisualsManager.Instance.SpawnCardObject(card2, card2.CardParent);
-        }
-    }
-
-    private CardSO DrawCardFromDeck()
-    {
-        return PokerDeckManager.Instance.DrawCard();
-    }
-    private void InitializePlayers()
-    {
-        _playerHands = new List<PokerPlayerHand>
-        {
-            _playerHand,
-            _aiPlayerOneHand,
-            _aiPlayerTwoHand,
-            _aiPlayerThreeHand,
-            _aiPlayerFourHand
-        };
-    }
-
-    public List<List<CardSO>> GetAllPlayerHands()
-    {
-        List<List<CardSO>> allHands = new List<List<CardSO>>();
-        List<CardSO> communityCards = _communityCards.GetCardList();
-
-        // Add each player's combined hand to the list
-        allHands.Add(CombineHandWithCommunity(_playerHand.GetCardList(), communityCards));
-        allHands.Add(CombineHandWithCommunity(_aiPlayerOneHand.GetCardList(), communityCards));
-        allHands.Add(CombineHandWithCommunity(_aiPlayerTwoHand.GetCardList(), communityCards));
-        allHands.Add(CombineHandWithCommunity(_aiPlayerThreeHand.GetCardList(), communityCards));
-        allHands.Add(CombineHandWithCommunity(_aiPlayerFourHand.GetCardList(), communityCards));
-
-        return allHands;
-    }
-
-    private List<CardSO> CombineHandWithCommunity(List<CardSO> playerHand, List<CardSO> communityCards)
-    {
-        List<CardSO> combinedHand = new List<CardSO>(playerHand);
-        combinedHand.AddRange(communityCards);
-        return combinedHand;
     }
 }
--- PokerGame/Assets/Scripts/PokerDeckManager.cs
+++ PokerGame/Assets/Scripts/PokerDeckManager.cs
@@ -5,16 +5,49 @@ public class PokerDeckManager : MonoBehaviour
 {
     public static PokerDeckManager Instance { get; private set; }
 
+    private List<CardSO> _pokerDeck = new();
+    private List<CardSO> _discardPile = new();
+
     //deckSO is to initialize the card deck list
     [SerializeField] private DeckSO _deckSO;
 
-    private List<CardSO> _pokerDeck = new();
-    private List<CardSO> _discardPile = new();
+    [SerializeField] private CommunityCards _communityCards;
+    [SerializeField] private PokerPlayerHand _playerHand;
+    [SerializeField] private PokerPlayerHand _aiPlayerOneHand;
+    [SerializeField] private PokerPlayerHand _aiPlayerTwoHand;
+    [SerializeField] private PokerPlayerHand _aiPlayerThreeHand;
+    [SerializeField] private PokerPlayerHand _aiPlayerFourHand;
+    private List<PokerPlayerHand> _playerHands;
+
+    
     private void Awake()
     {
         Instance = this;
         LoadDeck();
         ShuffleDeck();
+    }
+
+    private void Start()
+    {
+        GameManager.OnGameStateChanged += GameManager_OnGameStateChanged;
+    }
+
+    private void GameManager_OnGameStateChanged(GameManager.GameState obj)
+    {
+        if(obj == GameManager.GameState.NewRound)
+        {
+            OnGameStart();
+        }
+    }
+
+    private void OnGameStart()
+    {
+        InitializePlayers();
+        DrawInitialCommunityCards();
+        DealCardsToPlayers();
+        List<List<CardSO>> allPlayerCards = GetAllPlayerHands();
+        PokerHandEvaluator.WinningHandResults winningHandResult = PokerHandEvaluator.Instance.EvaluateAndFindWinner(allPlayerCards);
+        Debug.Log("Winning hand type: " + winningHandResult.WinningHandType + "- Player Index(0,1,2,3,4), 0 is the player. : " + winningHandResult.WinningHandIndex + " - Winning Hand(5Cards) Ranks: " + winningHandResult.WinningCardCodes);
     }
 
     // Load all cards from DeckSO into the deck List
@@ -60,5 +93,85 @@ public class PokerDeckManager : MonoBehaviour
         _pokerDeck.AddRange(_discardPile); // Add all cards from discard pile back to the deck
         _discardPile.Clear(); // Clear the discard pile
         ShuffleDeck(); // Shuffle the deck
+    }
+
+    private void DrawInitialCommunityCards()
+    {
+        CardSO card1 = DrawCardFromDeck();
+        CardSO card2 = DrawCardFromDeck();
+        CardSO card3 = DrawCardFromDeck();
+
+        if (card1 != null)
+        {
+            _communityCards.AddCard(card1);
+            CardVisualsManager.Instance.SpawnCardObject(card1, card1.CardParent);
+
+        }
+        if (card2 != null)
+        {
+            _communityCards.AddCard(card2);
+            CardVisualsManager.Instance.SpawnCardObject(card2, card2.CardParent);
+        }
+        if (card3 != null)
+        {
+            _communityCards.AddCard(card3);
+            CardVisualsManager.Instance.SpawnCardObject(card3, card3.CardParent);
+        }
+    }
+
+    private void DealCardsToPlayers()
+    {
+        //Draws 2 cards for each player. 
+        foreach (PokerPlayerHand hand in _playerHands)
+        {
+            CardSO card1 = DrawCardFromDeck(); // Draw the first card
+            CardSO card2 = DrawCardFromDeck(); // Draw the second card
+
+            if (card1 != null)
+                hand.AddCard(card1); // Add the first card to the player's hand
+            CardVisualsManager.Instance.SpawnCardObject(card1, card1.CardParent);
+
+            if (card2 != null)
+                hand.AddCard(card2); // Add the second card to the player's hand
+            CardVisualsManager.Instance.SpawnCardObject(card2, card2.CardParent);
+        }
+    }
+
+    private CardSO DrawCardFromDeck()
+    {
+        return PokerDeckManager.Instance.DrawCard();
+    }
+    private void InitializePlayers()
+    {
+        _playerHands = new List<PokerPlayerHand>
+        {
+            _playerHand,
+            _aiPlayerOneHand,
+            _aiPlayerTwoHand,
+            _aiPlayerThreeHand,
+            _aiPlayerFourHand
+        };
+    }
+
+    public List<List<CardSO>> GetAllPlayerHands()
+    {
+        List<List<CardSO>> allHands = new List<List<CardSO>>();
+        List<CardSO> communityCards = _communityCards.GetCardList();
+
+        // Add each player's combined hand to the list
+        allHands.Add(CombineHandWithCommunity(_playerHand.GetCardList(), communityCards));
+        allHands.Add(CombineHandWithCommunity(_aiPlayerOneHand.GetCardList(), communityCards));
+        allHands.Add(CombineHandWithCommunity(_aiPlayerTwoHand.GetCardList(), communityCards));
+        allHands.Add(CombineHandWithCommunity(_aiPlayerThreeHand.GetCardList(), communityCards));
+        allHands.Add(CombineHandWithCommunity(_aiPlayerFourHand.GetCardList(), communityCards));
+
+        return allHands;
+    }
+
+    private List<CardSO> CombineHandWithCommunity(List<CardSO> playerHand, List<CardSO> communityCards)
+    {
+        List<CardSO> combinedHand = new List<CardSO>(playerHand);
+        combinedHand.AddRange(communityCards);
+        return combinedHand;
     }
 }
